2b76a2402a834e7d40c97425492d2f2802090df3
Rohitsharma7877 <rohitsharma.sharma194@gmail.com>
2023-07-27 02:12:49 +0530

    backendDone

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,11 @@ import authRoutes from './routes/auth.js'
 import userRoutes from './routes/users.js'
 import postRoutes from './routes/posts.js'
 import {register} from './controllers/auth.js'
-
+import {createPost} from './controllers/posts.js'
+import { verifyToken } from './middleware/auth.js';
+// import User from "./models/User.js";
+// import Post from "./models/Post.js";
+// import { users, posts } from "./data/index.js";
 
 
 // CONFIGURATION 
@@ -20,16 +24,15 @@ import {register} from './controllers/auth.js'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
-const app = express()
+const app = express();
 app.use(express.json());
-app.use(helmate())
-app.use(helmate.crossOriginResourcePolicy({policy:"cross-origin"}));
-app.use(morgan("common"))
-app.use(bodyParser.json({limit:"30mb",extended:true}));
-app.use(bodyParser.urlencoded({limit:'30mb', extend:true }));
+app.use(helmate());
+app.use(helmate.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
-
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 //FILE STORAGE
 
 const storage = multer.diskStorage({
@@ -45,11 +48,13 @@ const upload= multer({storage})
 
 // routes with file
 app.post('/auth/register', upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost );
 
 // routes with file
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use('/posts', postRoutes)
+
 
 
 // MONGOO CONNECT
